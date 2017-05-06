7bbd5b4c297818f17c4ed8089a851f73e3979b16
ThaiBinh <thaibinh719@gmail.com>
2017-05-06 21:42:52 +0700

    header style

--- src/components/Main/Shop/Header.js
+++ src/components/Main/Shop/Header.js
@@ -1,17 +1,53 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, Dimensions } from 'react-native';
-const { height } = Dimensions.get('window');
+import React, {Component} from 'react';
+import {View, Text, TouchableOpacity, Dimensions, Image, TextInput, StyleSheet } from 'react-native';
+
+import icLogo from '../../../media/appIcon/ic_logo.png';
+import icMenu from '../../../media/appIcon/ic_menu.png';
+
+const {height} = Dimensions.get('window');
 
 export default class Header extends Component {
     render() {
+        const {wrapper, row1, textInput, iconStyle, titleStyle} = styles;
         return (
-            <View style={{ height: height / 8 }} >
+            <View style={wrapper}>
+                <View style={row1}>
                     <TouchableOpacity onPress={this.props.onOpen} >
-                        <Text>
-                            Open Menu
-                        </Text>
+                        <Image source={icMenu} style={iconStyle} />
                     </TouchableOpacity>
+                    <Text style={titleStyle}>
+                        Wearing a Dress
+                    </Text>
+                    <Image source={icLogo} style={iconStyle} />
                 </View>
+                <TextInput style={textInput} placeholder='What do you want to buy?' />
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    wrapper:{
+        height: height / 8,
+        backgroundColor: '#2AA188',
+        padding:10,
+        justifyContent: 'space-between'
+    },
+    row1:{
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    textInput:{
+        height: height/18,
+        backgroundColor:'#fff',
+        paddingLeft:10,
+    },
+    iconStyle:{
+        width:20,
+        height:20
+    },
+    titleStyle:{
+        color:'#fff',
+        fontSize: 15
+    }
+});
