79d8d6114bb409600c47ea07464fc54609f46213
ThaiBinh <thaibinh719@gmail.com>
2017-05-08 14:47:45 +0700

    ket thuc buoi 2

--- src/components/Main/Shop/Home/Category.js
+++ src/components/Main/Shop/Home/Category.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
-import bannerImage from '../../../../media/temp/banner.jpg';
+import Swiper from 'react-native-swiper';
+
+import littleImage from '../../../../media/temp/little.jpg';
+import maxiImage from '../../../../media/temp/maxi.jpg';
+import partyImage from '../../../../media/temp/party.jpg';
+import midiImage from '../../../../media/temp/midi.jpg';
 
 const {width, height } = Dimensions.get('window');
 
 export default class Category extends Component {
     render() {
-        const {wrapper, textStyle, imageStyle}  = styles;
+        const {wrapper, textStyle, imageStyle,cateTitle}  = styles;
         return (
             <View style={wrapper}>
                 <View style={{flex:1, justifyContent:'center'}}>
                     <Text style={textStyle}>
-                        SPRING COLLECTION
+                        LIST OF CATEGORY
                     </Text>
                 </View>
                 <View style={{flex:4}}>
-                    <Image source={bannerImage} style={imageStyle}/>
+                    <Swiper width={imageWidth} height={imageHeight}>
+                        <Image source={littleImage} style={imageStyle}>
+                            <Text style={cateTitle}>Little Dress</Text>
+                        </Image>
+                        <Image source={midiImage} style={imageStyle}>
+                            <Text style={cateTitle}>Midi Dress</Text>
+                        </Image>
+                        <Image source={maxiImage} style={imageStyle}>
+                            <Text style={cateTitle}>Maxi Dress</Text>
+                        </Image>
+                        <Image source={partyImage} style={imageStyle}>
+                            <Text style={cateTitle}>Party Dress</Text>
+                        </Image>
+                    </Swiper>
                 </View>
             </View>
         );
@@ -42,6 +60,12 @@ const styles = StyleSheet.create({
     },
     imageStyle:{
         height:imageHeight,
-        width:imageWidth
+        width:imageWidth,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    cateTitle:{
+        fontSize:15,
+        color:'#B0B0B0'
     }
 });
